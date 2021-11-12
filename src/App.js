96de2f4814b87795bc4876de631e48a7df50aa19
geethu <geethukallada1@gmail.com>
2021-11-12 10:46:29 +0530

    counter_setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[count,setCount]=useState(0);     
   return (
      <div className="App">   
-      <h1></h1>
+      <div 
+      className="count_button"
+      onClick={
+        ()=>{
+          setCount(count-1);
+        }
+      }
+      >
+        -
+      </div>
+      <div className="count_value">
+        {count}
+      </div>
+
+      <div 
+      className="count_button"
+      onClick={
+        ()=>{
+          setCount(count+1);
+        }
+      }
+      >
+        +
+      </div>
+
     </div>
   );
 }
