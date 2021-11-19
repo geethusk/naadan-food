3a4af6a2793512444054fe0849486cdce1127937
geethu <geethukallada1@gmail.com>
2021-11-19 12:01:20 +0530

    todolist

--- src/Learn.jsx
+++ src/Learn.jsx
@@ -6,31 +6,45 @@ import './index.css'
 const Learn = () => {
     const [toDoList, setToDoList ] = useState([]);
     const [toDo, setToDo] = useState("");
+
         const addToDoList=()=>{
-             setToDoList(prev=>[...prev,toDo])     //we always take previous array for  storing
-            setToDo("")        //to clear the text box once u done with the procedure .
+             setToDoList(prev=>[...prev,toDo])     
+            setToDo("")        
         }
     return (
-        <div>
+        <div className="container">
             <div className="to-do-list">
-                <h1>Todo</h1>
-                <h3> Hello World! A To Do List Will Go here!</h3>
-                <input type="text" value={toDo} placeholder="Type Here..."
+                <h1>Todos</h1>
+                <div className="input-section">
+                <input className="todo-area" type="text" value={toDo} placeholder="Type Here..."
                  onChange={(e)=>{
                 setToDo(e.target.value)
             }}/>
             <button className="to-do-add" onClick={addToDoList}> 
-                done</button>
+                ADD</button></div>
+            
 
-            <ul id="list">
-                {toDoList.map((value,i)=><li>     
-                onClick={()=>{
-                    setToDoList(toDoList.filter((_value,index)=>
-                     i!==index    //to check whether the data's are correct are not 
-                    ))     
-                }}
-                {value}</li>)}
-                </ul>
+              <ul id="list">
+                    {toDoList.map((value,i)=><li>
+                        <div className="status-circle active not-active"
+                        onClick={()=>{
+                            
+                        }}
+                        />
+                    <div className="todo-text">
+                    {value}
+                    </div>
+                    <div className="to-do-close-button"
+                         onClick={()=>{
+                            setToDoList(toDoList.filter((_value,index)=>
+                            i!==index    
+                            ))     
+                        }}
+                    />
+                    </li>)}
+
+
+              </ul>
             </div>
           
 
