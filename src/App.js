115549bc3f2e180c3016d4246ef1339d35f2780f
geethu <geethukallada1@gmail.com>
2021-11-19 12:07:29 +0530

    todolist

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import {
 } from "react-router-dom";
 import Learn from "./Learn";
 
+
 const App = () => {
   return (
    <Router>
      <Switch>
-       <Route path="/learn" component={Learn}/>
+      <Route path="/learn" component={Learn}/>
        <Route path="/" component={Home}/>
        
     </Switch>
